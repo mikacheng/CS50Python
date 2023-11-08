71ea18bd64be912b01056702bd678c9e9bd53294
MC CS50 <contactmickael@yahoo.fr>
2023-11-08 21:42:25 +0000

    update

--- sequences.py
+++ sequences.py
@@ -3,6 +3,9 @@ name = "Harry"
 for i in range(0, len(name)):
   print(name[i])
 
+# Define a list of names
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+
 
 
   ''' list  -  Sequence of mutable values
