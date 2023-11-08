0e263b99db1519937f8a51d5206d08480bea8126
MC CS50 <contactmickael@yahoo.fr>
2023-11-08 21:29:28 +0000

    seq

---
+++ sequences.py
@@ -0,0 +1,12 @@
+name = "Harry"
+
+for i in range(0, len(name)):
+  print(name[i])
+
+
+
+  ''' list  -  Sequence of mutable values
+      tuple -  Sequence of immutable values
+      set   -  Sequence of unique values
+      dict  -  Sequence of key-value pairs
+  '''
